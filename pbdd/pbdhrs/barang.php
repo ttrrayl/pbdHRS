26103512e5bb1c03360313dc77af8894b3410cc7
Tri Ayunia Patma Lubis <77679461+ttrrayl@users.noreply.github.com>
2022-06-05 16:39:56 +0700

    tambah antrian

---
+++ pbdd/pbdhrs/barang.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+    
+        <title>HRS - Stok Barang</title>
+    
+        <!-- Custom fonts for this template-->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
+    
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    
+</head>
+
+
+
+<body class="bg-gray-200">
+
+    <div id = "content">
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow ">
+            <h1 class="h3 my-3 text-gray-800">HRS Car Wash and Services</h1>
+        </nav>
+
+    <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Stok Barang</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Nama Item</th>
+                                    <th>Jenis Item</th>
+                                    <th>Jumlah</th>
+                                    <th>Harga</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Motul</td>
+                                    <td>Oli Motor</td>
+                                    <td>5</td>
+                                    <td>Rp.80000</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>GoodYear</td>
+                                    <td>Ban Mobil</td>
+                                    <td>15</td>
+                                    <td>Rp.2000000</td>
+                                </tr>
+
+                            </tbody>
+                        </table>  
+                    </div>    
+                </div>
+            </div>
+            
+            <a href="menu.php" class="btn btn-danger btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-arrow-right"></i>
+                </span>
+                <span class="text">Kembali</span>
+            </a>
+        </div>  
+      
+    </div>
+    
+</body>
+</html>
